4e21d12bd82fd0cd8f6c40d3f76b48a2cb920366
Cornee van Blerk <corneevanblerk@gmail.com>
2023-11-04 11:50:21 +0100

    fix: removed booboo

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,19 +2,19 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char *SSID = "Rainbow Road";
-const char *PWD = "IAmSpeed69!";
+const char *SSID = nullptr;
+const char *PWD = nullptr;
 
 const char *SERVER = "192.168.178.214";
 const int PORT = 1883;
 
-#define DHT_SENSOR_PIN  4
+#define DHT_SENSOR_PIN 4
 #define DHT_SENSOR_TYPE DHT22
 
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
 WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient); 
+PubSubClient mqttClient(wifiClient);
 
 void connectToWiFi() {
   Serial.print("Connecting to ");
@@ -37,26 +37,36 @@ void setupMQTT() {
 void reconnect() {
   Serial.println("Connecting to MQTT Broker");
   while (!mqttClient.connected()) {
-      Serial.println("Reconnecting to MQTT Broker..");
-      String clientId = "ESP32Client-";
-      clientId += String(random(0xffff), HEX);
-      
-      if (mqttClient.connect(clientId.c_str())) {
-        Serial.println("Connected.");
-      }
-      
-      delay(500);
+    Serial.println("Reconnecting to MQTT Broker..");
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+
+    if (mqttClient.connect(clientId.c_str())) {
+      Serial.println("Connected.");
+    }
+
+    delay(500);
   }
 }
 
 void setup() {
   Serial.begin(115200);
 
+  SSID = getenv("WIFI_SSID");
+  PWD = getenv("WIFI_PASSWORD");
+
+  if (SSID == nullptr || PWD == nullptr) {
+    Serial.println("Error: SSID and password not set in environment variables.");
+    while (1) {
+      delay(1000);
+    }
+  }
+
   connectToWiFi();
 
   setupMQTT();
-  
-  dht_sensor.begin(); 
+
+  dht_sensor.begin();
 }
 
 void loop() {
@@ -64,7 +74,7 @@ void loop() {
     reconnect();
   mqttClient.loop();
 
-  float humi  = dht_sensor.readHumidity();
+  float humi = dht_sensor.readHumidity();
   float tempC = dht_sensor.readTemperature();
   float abs = dht_sensor.computeHeatIndex(tempC, humi, false);
 
